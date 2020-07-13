1bc54a34a3f567861412f0b5a10099f4f09cea86
Salman Zafar <salman.zafar@innowi.com>
2020-07-13 18:59:46 +0500

    added support ticket section

--- database/factories/SupportTicketFactory.php
+++ database/factories/SupportTicketFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(SupportTicket::class, function (Faker $faker) {
     return [
-        //
+        'question' => $faker->paragraph
     ];
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <div class="flex-center position-ref full-height">
                 @livewire('counter')
                 @livewire('comments')
+                @livewire('tickets')
                 {{--                //<livewire:comments :comments="$comments"/>--}}
 
 </div>
