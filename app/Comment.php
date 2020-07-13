afa65ccfd6708e1bb76fc04acb83dc712514822a
Salman Zafar <salman.zafar@innowi.com>
2020-07-13 16:16:02 +0500

    working on support ticket

--- app/Comment.php
+++ app/Comment.php
@@ -15,7 +15,15 @@ class Comment extends Model
 
     public function user()
     {
-        return $this
-            ->belongsTo(User::class);
+        return
+            $this
+                ->belongsTo(User::class);
+    }
+
+    public function support_ticket()
+    {
+        return
+            $this
+                ->belongsTo(SupportTicket::class);
     }
 }
---
+++ app/SupportTicket.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupportTicket extends Model
+{
+    protected $fillable = ['question'];
+
+    public function comments()
+    {
+        return
+            $this
+                ->hasMany(Comment::class);
+    }
+}
